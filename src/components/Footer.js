5d0b47bc83090ec970ebd3c348e8ce9359bedcc0
siblibusro87 <60931940+siblibusro87@users.noreply.github.com>
2020-09-20 01:03:31 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/siblibusro/">@siblibusro</a>
     </h2>
     <br />
     <footer className="footer">
