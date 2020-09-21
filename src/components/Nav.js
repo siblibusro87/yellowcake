dc266eadfc5e73072ec24f5180f534fab52abad0
siblibusro87 <60931940+siblibusro87@users.noreply.github.com>
2020-09-21 14:59:45 +0700

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -82,7 +82,7 @@ export class Navigation extends Component {
                 </div>
               </span>
             </div>
-            <NavLink to="/about-us/">About Us</NavLink>
+            <NavLink to="/about/">About Us</NavLink>
             <NavLink to="/contact/">Contact</NavLink>
           </div>
           <button
