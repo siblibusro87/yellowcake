dfcaf56ec26b322920295289ab407b9d36f15264
siblibusro87 <60931940+siblibusro87@users.noreply.github.com>
2020-09-22 22:04:53 +0700

    Update SinglePost.js

--- src/templates/SinglePost.js
+++ src/templates/SinglePost.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import _get from 'lodash/get'
 import { Link, graphql } from 'gatsby'
-import { ChevronLeft } from 'react-feather'
+
 
 import Content from '../components/Content'
 import Layout from '../components/Layout'
@@ -22,9 +22,7 @@ export const SinglePostTemplate = ({
       itemType="http://schema.org/BlogPosting"
     >
       <div className="container skinny">
-        <Link className="SinglePost--BackButton" to="/blog/">
-          <ChevronLeft /> BACK
-        </Link>
+        
         <div className="SinglePost--Content relative">
           <div className="SinglePost--Meta">
             {date && (
