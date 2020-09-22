0d61a91e1d073262809f634045d134f2095d63c6
siblibusro87 <60931940+siblibusro87@users.noreply.github.com>
2020-09-22 18:56:53 +0700

    Update PostCategoriesNav.js

--- src/components/PostCategoriesNav.js
+++ src/components/PostCategoriesNav.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import BlogSearch from './BlogSearch'
 import './PostCategoriesNav.css'
 
 const PostCategoriesNav = ({ categories, enableSearch }) => (
   <div className="PostCategoriesNav">
-           {enableSearch && <BlogSearch />}
+       {enableSearch && <BlogSearch />}
   </div>
 )
 
