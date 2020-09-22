085100f98c3b35375005426859dc9e999d15e63f
siblibusro87 <60931940+siblibusro87@users.noreply.github.com>
2020-09-22 18:52:42 +0700

    Update PostCategoriesNav.js

--- src/components/PostCategoriesNav.js
+++ src/components/PostCategoriesNav.js
@@ -6,21 +6,7 @@ import './PostCategoriesNav.css'
 
 const PostCategoriesNav = ({ categories, enableSearch }) => (
   <div className="PostCategoriesNav">
-    <Link className="NavLink" exact="true" to={`/blog/`}>
-      All
-    </Link>
-    {categories.map((category, index) => (
-      <Link
-        exact="true"
-        className="NavLink"
-        key={category.title + index}
-        to={category.slug}
-      >
-        {category.title}
-      </Link>
-    ))}
-
-    {enableSearch && <BlogSearch />}
+           {enableSearch && <BlogSearch />}
   </div>
 )
 
